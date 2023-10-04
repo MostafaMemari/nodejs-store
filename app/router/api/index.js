3f349523eb5b9de3fa35e5fde14a8da4cd308766
MostafaMemari <144701776+MostafaMemari@users.noreply.github.com>
2023-10-04 17:02:27 +0330

    config swagger

--- app/router/api/index.js
+++ app/router/api/index.js
@@ -1,8 +1,26 @@
-const homeController = require("../../http/controllers/api/home.controller");
-
 const router = require("express").Router();
+const homeController = require("../../http/controllers/api/home.controller");
+/**
+ * @swagger
+ * tags:
+ *  name : IndexPage
+ *  description : index page route and data
+ */
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    summary: index of routes
+ *    tags : [IndexPage]
+ *    description: get All need data for index page
+ *    responses:
+ *      200:
+ *        description: success
+ *      404:
+ *        description: not found
+ */
 
-router.post("/", homeController.indexPage);
+router.get("/", homeController.indexPage);
 
 module.exports = {
   HomeRoutes: router,
--- app/server.js
+++ app/server.js
@@ -4,6 +4,8 @@ const path = require("path");
 const { AllRouter } = require("./router/router");
 const morgan = require("morgan");
 const createError = require("http-errors");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 module.exports = class Application {
   #app = express();
   #DB_URI;
@@ -22,6 +24,32 @@ module.exports = class Application {
     this.#app.use(express.json());
     this.#app.use(express.urlencoded({ extended: true }));
     this.#app.use(express.static(path.join(__dirname, "..", "public")));
+    this.#app.use(
+      "/api-doc",
+      swaggerUI.serve,
+      swaggerUI.setup(
+        swaggerJsDoc({
+          swaggerDefinition: {
+            info: {
+              title: "store shop",
+              version: "1.0.0",
+              description: "بزرگترین مرجع فروشگاهی ایران",
+              contact: {
+                name: "Mostafa Memari",
+                url: "https://www.google.com",
+                email: "m.memari7@yahoo.com",
+              },
+            },
+            servers: [
+              {
+                url: "http://localhost:5000",
+              },
+            ],
+          },
+          apis: ["./app/router/*/*.js"],
+        })
+      )
+    );
   }
   createServer() {
     const http = require("http");
