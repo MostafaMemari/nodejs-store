96b110e9d7849c9b254166680e7986ed12687210
MostafaMemari <144701776+MostafaMemari@users.noreply.github.com>
2023-10-04 18:11:25 +0330

    config user login swagger And OTP

--- app/http/controllers/user/auth/auth.controller.js
+++ app/http/controllers/user/auth/auth.controller.js
@@ -1,14 +1,51 @@
 const createHttpError = require("http-errors");
 const { authSchema } = require("../../../validators/user/auth.schema");
+const { randomNumberGenerator } = require("../../../../utils/functions");
+const { UserModel } = require("../../../../models/user");
+const { EXPIRES_IN, USER_ROLE } = require("../../../../utils/constans");
+const Controller = require("../../controller");
 
-class UserAuthController {
+class UserAuthController extends Controller {
   async login(req, res, next) {
     try {
-      const result = await authSchema.validateAsync(req.body);
-      return res.status(200).send("ورود شما با موفقیت انجام شد");
+      await authSchema.validateAsync(req.body);
+      const { mobile } = req.body;
+      const code = randomNumberGenerator();
+      const result = await this.saveUser(mobile, code);
+      if (!result) throw createHttpError.Unauthorized("ورود شما انجام نشد");
+      return res.status(200).send({
+        data: {
+          statusCode: 200,
+          message: "کد اعتبار سنجی با موفقیت برای شما ارسال شد",
+          code,
+          mobile,
+        },
+      });
     } catch (error) {
       next(createHttpError.BadRequest(error.message));
     }
+  }
+  async saveUser(mobile, code) {
+    let otp = {
+      code,
+      expiresIn: EXPIRES_IN,
+    };
+    const result = await this.checkExistUser(mobile);
+    if (result) {
+      return await this.updateUser(mobile, { otp });
+    }
+    return !!(await UserModel.create({ mobile, otp, roles: [USER_ROLE] }));
+  }
+  async checkExistUser(mobile) {
+    const user = await UserModel.findOne({ mobile });
+    return !!user;
+  }
+  async updateUser(mobile, objectData = {}) {
+    Object.keys(objectData).forEach((key) => {
+      if (["", " ", 0, null, undefined, "0", NaN].includes(objectData[key])) delete objectData[key];
+    });
+    const updateResult = await UserModel.updateOne({ mobile }, { $set: objectData });
+    return !!updateResult.modifiedCount;
   }
 }
 
--- app/router/user/auth.js
+++ app/router/user/auth.js
@@ -1,6 +1,35 @@
 const { UserAuthController } = require("../../http/controllers/user/auth/auth.controller");
 
 const router = require("express").Router();
+/**
+ * @swagger
+ * tags:
+ *  name : User-Authentication
+ *  description : user-auth section
+ */
+/**
+ * @swagger
+ *  /user/login:
+ *    post:
+ *      tags: [User-Authentication]
+ *      summary: login user in userpanel with phone number
+ *      description: one time password (OTP) login
+ *      parameters:
+ *      - name: mobile
+ *        description: fa-IRI phone number
+ *        in: formData
+ *        required: true
+ *        type: string
+ *      responses:
+ *        201:
+ *          description: Success
+ *        400:
+ *          description: Bad requiest
+ *        401:
+ *          description: Inauthorization
+ *        500:
+ *          description: Internal server error
+ */
 
 router.post("/login", UserAuthController.login);
 
