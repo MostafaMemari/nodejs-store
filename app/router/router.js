238d257b929389d4add6eedc1ff445a3b16fc50a
MostafaMemari <144701776+MostafaMemari@users.noreply.github.com>
2023-10-04 16:31:40 +0330

    start route auth login

--- app/router/router.js
+++ app/router/router.js
@@ -1,7 +1,9 @@
 const { HomeRoutes } = require("./api");
+const { UserAuthRouter } = require("./user/auth");
 
 const router = require("express").Router();
 
+router.use("/user", UserAuthRouter);
 router.use("/", HomeRoutes);
 
 module.exports = {
