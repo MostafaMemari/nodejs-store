bd7af563a5ceff41de5dfb2e4d68d7f705034350
MostafaMemari <144701776+MostafaMemari@users.noreply.github.com>
2023-10-11 17:46:44 +0330

    update product swagger route

--- app/router/admin/product.js
+++ app/router/admin/product.js
@@ -94,9 +94,69 @@ const router = require("express").Router();
 
 /**
  * @swagger
+ *  components:
+ *    schemas:
+ *      Edit-Product:
+ *        type: object
+ *        properties:
+ *          title:
+ *            type: string
+ *            description: the title of products
+ *          short_text:
+ *            type: string
+ *            description: the short_text of products
+ *          text:
+ *            type: string
+ *            description: the text of products
+ *          tags:
+ *            type: array
+ *            description: the tags of products
+ *          category:
+ *            type: string
+ *            description: the category of products
+ *          price:
+ *            type: string
+ *            description: the price of products
+ *          count:
+ *            type: string
+ *            description: the count of products
+ *          discount:
+ *            type: string
+ *            description: the discount of products
+ *          images:
+ *            type: array
+ *            items:
+ *              type: string
+ *              format: binary
+ *          height:
+ *            type: string
+ *            description: the height of products
+ *            example: 0
+ *          weight:
+ *            type: string
+ *            description: the weight of products
+ *            example: 0
+ *          width:
+ *            type: string
+ *            description: the with of products
+ *            example: 0
+ *          length:
+ *            type: string
+ *            description: the length of products
+ *            example: 0
+ *          type:
+ *            type: string
+ *            description: the type of product
+ *            example: virtual - physical
+ *          colors:
+ *            $ref: '#/components/schemas/Color'
+ */
+
+/**
+ * @swagger
  * /admin/products/add:
  *  post:
- *    tags : [Product(AdminPanel)]
+ *    tags: [Product(AdminPanel)]
  *    summary: create and save product
  *    requestBody:
  *      required: true
@@ -110,6 +170,32 @@ const router = require("express").Router();
  *
  */
 router.post("/add", uploadFile.array("images", 10), stringToArray("tags"), stringToArray("colors"), ProductController.addProduct);
+
+/**
+ * @swagger
+ * /admin/products/edit/{id}:
+ *  patch:
+ *    tags: [Product(AdminPanel)]
+ *    summary: update product By ID
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        multipart/form-data:
+ *          schema:
+ *            $ref: "#/components/schemas/Edit-Product"
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        description: id of product for update
+ *        required: true
+ *        type: string
+
+ *    responses:
+ *      201:
+ *        description: updated product successfully
+ *
+ */
+router.patch("/products/edit/:id", uploadFile.array("images", 10), stringToArray("tags", "colors"), ProductController.editProduct);
 
 /**
  * @swagger
