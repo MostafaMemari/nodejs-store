174dfbf5dca1dda59b78678277e82e1c7b7aab21
MostafaMemari <144701776+MostafaMemari@users.noreply.github.com>
2023-10-19 16:00:56 +0330

    create route update user profile

--- app/http/controllers/admin/user/user.controller.js
+++ app/http/controllers/admin/user/user.controller.js
@@ -1,6 +1,8 @@
 const { StatusCodes } = require("http-status-codes");
 const { UserModel } = require("../../../../models/user");
 const Controller = require("../../controller");
+const { deleteInvalidPropertyInObject } = require("../../../../utils/functions");
+const createHttpError = require("http-errors");
 
 class UserController extends Controller {
   async getAllUsers(req, res, next) {
@@ -17,6 +19,30 @@ class UserController extends Controller {
           users,
         },
       });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async updateUserProfile(req, res, next) {
+    try {
+      const userID = req.user._id;
+      const data = req.body;
+      const BlackListFields = ["mobile", "otp", "bills", "discount", "Roles", "Courses"];
+      deleteInvalidPropertyInObject(data, BlackListFields);
+      const profileUpdateResult = await UserModel.updateOne({ _id: userID }, { $set: data });
+      if (!profileUpdateResult.modifiedCount) throw new createHttpError.InternalServerError("به روزسانی انجام نشد");
+      return res.status(StatusCodes.OK).json({
+        statusCode: StatusCodes.OK,
+        data: {
+          message: "به روزرسانی پروفایل با موفقیت انجام شد",
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async userProfile(req, res, next) {
+    try {
     } catch (error) {
       next(error);
     }
